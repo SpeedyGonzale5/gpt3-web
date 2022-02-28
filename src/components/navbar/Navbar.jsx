4688d53a9f5a087c776369d3f1c7224a28a5fe38
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-02-28 00:34:37 -0500

    February 28, 2022 12:34 AM

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,10 +13,10 @@ import logo from '../../assets/logo.svg'
 </div>
 <div className='gpt3__navbar-links_container'>
   <p><a href="#home">Home</a></p>
-  <p><a href="#home">What is GPT?</a></p>
-  <p><a href="#home">Open AI</a></p>
-  <p><a href="#home">Case Studies</a></p>
-  <p><a href="#home">Case Studies</a></p>
+  <p><a href="#wgpt3">What is GPT?</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Case Studies</a></p>
 
 </div>
         </div>
