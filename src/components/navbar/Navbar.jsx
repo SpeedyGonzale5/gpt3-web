c87535d816a0e21c507c8c731fdb6499b02eeb2f
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-02-28 00:43:43 -0500

    February 28, 2022 12:43 AM

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,7 +16,7 @@ import logo from '../../assets/logo.svg'
   <p><a href="#wgpt3">What is GPT?</a></p>
   <p><a href="#possibility">Open AI</a></p>
   <p><a href="#features">Case Studies</a></p>
-  <p><a href="#blog">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
 
 </div>
         </div>
