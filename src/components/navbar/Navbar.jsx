d1772cd1780e69820fe562685d4766925a06b0c8
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-02-28 13:38:52 -0500

    February 28, 2022 1:38 PM

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,15 +16,18 @@ const Menu = () => (
 // BEM -< Block Element Modifier
  const Navbar = () => {
    const [toggleMenu, setToggleMenu] = useState(false);
-  
-   return (
+  return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
 <div className='gpt3__navbar-links_logo'>
   <img src= {logo} alt="logo"></img>
 </div>
 <div className='gpt3__navbar-links_container'>
-  <Menu />
+  <p><a href="#home">Home</a></p>
+  <p><a href="#wgpt3">What is GPT?</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
 </div>
         </div>
         <div className='gpt3__navbar-sign'>
@@ -33,18 +36,12 @@ const Menu = () => (
         </div>
         <div className='gpt3__navbar-menu'>
           {toggleMenu
-            ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/> 
+            ? <RiCloseLine color="#fff" size={27 } onClick={() => setToggleMenu(false)}/> 
             : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/> 
           };
           {toggleMenu && (
             <div classname="gpt3__navbar-menu_container scale-up-center">
-              <div className="gpt3__navbar-menu_container-links">
-              <Menu />
-              <div className='gpt3__navbar-menu_container-links-sign'>
-          <p>Sign In</p>
-          <button type="button">Sign up</button>
-        </div>
-              </div>
+              <div className="gpt3__navbar-menu_container-links"></div>
             </div>
           )}
         </div>
