64245a6fc85e17c70a3d6b0ca47b4a519c5e2f8f
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 22:28:51 -0400

    March 17, 2022 10:28 PM

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -9,16 +9,16 @@ const Brand = () => {
         <img src={google} alt="google" />
       </div>
       <div>
-        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
       </div>
       <div>
-        <img src={google} alt="google" />
+        <img src={atlassian} alt="atlassian" />
       </div>
       <div>
-        <img src={google} alt="google" />
+        <img src={dropbox} alt="dropbox" />
       </div>
       <div>
-        <img src={google} alt="google" />
+        <img src={shopify} alt="shopify" />
       </div>
     </div>
   )
