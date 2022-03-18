67e56b8f9ba834ad9cd05f2c399542a1f9084590
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 20:01:25 -0400

    March 17, 2022 8:01 PM

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,13 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <div classname ="gpt3__header section__padding" id ="home">Header</div>
+    <div classname ="gpt3__header section__padding" id ="home">
+      <div classname ="gpt3__header-content">
+        <h1 classname ="gradient__text">Let’s Build Something
+amazing with GPT-3
+OpenAI</h1>
+      </div>
+    </div>
   )
 }
 
