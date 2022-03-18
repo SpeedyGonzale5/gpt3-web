bca662347f47c6fa96fdb2b81ee6e593b9fa0781
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 22:55:10 -0400

    March 17, 2022 10:55 PM

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -9,7 +9,7 @@ import "./whatGPT3.css";
         <Feature/>
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
