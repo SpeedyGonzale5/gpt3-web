74feb05e35befa9d54d313c46300ead9ea8965b6
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 20:11:50 -0400

    March 17, 2022 8:11 PM

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,6 +6,11 @@ const Header = () => {
     <div className ="gpt3__header section__padding" id ="home">
       <div className ="gpt3__header-content">
         <h1 className ="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+
+        <div className ="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address"></input>
+          <button type="button" >Get Started</button>
+        </div>
       </div>
     </div>
   )
