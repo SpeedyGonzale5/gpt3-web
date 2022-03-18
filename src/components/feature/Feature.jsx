5f62fec28649737f7f6c26e55accf4d974b4ce74
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 23:23:42 -0400

    March 17, 2022 11:23 PM

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,12 +3,12 @@ import "./feature.css";
 
  const Feature = ( { title, text } ) => {
   return (
-    <div className="gtp3__features-container__feature">
-      <div className="gtp3__features-container__feature-title">
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
         <div></div>
         <h1>{title}</h1>
       </div>
-      <div className="gtp3__features-container__feature-text">
+      <div className="gpt3__features-container__feature-text">
         {text}
       </div>
     </div>
