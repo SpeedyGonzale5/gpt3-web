fe2bde14e1003cdbea0c363848702cbe5ce77c33
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 22:18:59 -0400

    March 17, 2022 10:18 PM

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,10 +19,10 @@ const Header = () => {
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
 
+      </div>
         <div className ="gpt3__header-image">
           <img src={ai} alt="ai"></img>
         </div>
-      </div>
     </div>
   )
 }
