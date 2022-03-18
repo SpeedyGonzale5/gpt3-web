f7115c919fff089a51929637da30ad738c1ea346
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-17 23:06:32 -0400

    March 17, 2022 11:06 PM

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./feature.css";
 
- const Feature = () => {
+ const Feature = ( { title, text } ) => {
   return (
     <div className="gtp3__features-container__feature">
       <div className="gtp3__features-container__feature-title">
         <div></div>
         <h1>{title}</h1>
+      </div>
+      <div className="gtp3__features-container__feature-text">
+        {text}
       </div>
     </div>
   )
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -13,7 +13,7 @@ import "./whatGPT3.css";
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
         <Feature />
         <Feature />
       </div>
