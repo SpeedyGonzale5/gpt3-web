14e549cb2a95a527e7d1653f256c98c5bd10556b
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-26 18:03:29 -0400

    March 26, 2022 6:03 PM

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,7 +3,11 @@ import "./possibility.css";
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gtp3__possibility-image">
+        <img src={possibilityImage} alt="possibility"></img>
+      </div>
+    </div>
   )
 }
 export default Possibility;
