c8c47df2829477a32b0f2c141a86158234f56d9a
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-26 00:08:55 -0400

    March 26, 2022 12:08 AM

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,7 +3,7 @@ import "./features.css";
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div classname="gtp3__features section__padding" id = "features">Features</div>
   )
 }
 
