a39118cfdbaf3d934916b3088f7e68f696629343
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-26 00:11:43 -0400

    March 26, 2022 12:11 AM

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,7 +3,11 @@ import "./features.css";
 
 const Features = () => {
   return (
-    <div classname="gtp3__features section__padding" id = "features">Features</div>
+    <div classname="gpt3__features section__padding" id = "features">
+      <div classname="gpt3__features-heading">
+        <h1 classname="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+      </div>
+    </div>
   )
 }
 
