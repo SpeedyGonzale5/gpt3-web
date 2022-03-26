8ae913aad640db1601ff2cabdf72ed670f7edf3b
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-26 18:07:08 -0400

    March 26, 2022 6:07 PM

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -12,9 +12,10 @@ const Possibility = () => {
       <div className="gtp3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
         <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
-        <p></p>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
   )
 }
-export default Possibility;
+export default Possibility; 
