3a6473e58130ac39b65788f7461c534115d8d5d1
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-27 15:31:07 -0400

    March 27, 2022 3:31 PM

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -7,7 +7,12 @@ const Article = ({imgUrl, date, title}) => {
       <div className="gpt3__blog-container_article-image">
         <img src={imgUrl} alt="blog" />
       </div>
-      <div className="gpt3__blog-container_article-content"></div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+      </div>
     </div>
   )
 }
