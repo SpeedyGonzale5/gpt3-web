cfded06df311395517904530134f1caa5fe8f11e
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-27 15:26:51 -0400

    March 27, 2022 3:26 PM

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./article.css";
 
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container-article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+    </div>
   )
 }
 
