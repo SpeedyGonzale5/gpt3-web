50c4216ab4ab131334ead2b546bf19d6a3faf160
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-27 16:46:16 -0400

    March 27, 2022 4:46 PM

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,11 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text-">Do you want to step in to the future before others</h1>
+      </div>
+    </div>
   )
 }
 
