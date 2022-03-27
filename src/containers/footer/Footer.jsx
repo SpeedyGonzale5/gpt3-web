1bc98b85502b92742f489de50771b72daafb5100
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-03-27 16:48:13 -0400

    March 27, 2022 4:48 PM

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,7 +5,17 @@ const Footer = () => {
   return (
     <div className="gpt3__footer section__padding">
       <div className="gpt3__footer-heading">
-        <h1 className="gradient__text-">Do you want to step in to the future before others</h1>
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+      
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access </p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo}></img>
+        </div>
       </div>
     </div>
   )
