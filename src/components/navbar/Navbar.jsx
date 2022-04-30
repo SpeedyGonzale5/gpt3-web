57fba88066a590f56bb2fb749346cd76ed250de6
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-04-30 18:56:22 -0400

    April 30, 2022 6:56 PM

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import logo from '../../assets/logo2.png'
 
 const Menu = () => (
   <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#wgpt3">What is GPT?</a></p>
-  <p><a href="#possibility">Open AI</a></p>
-  <p><a href="#features">Case Studies</a></p>
-  <p><a href="#blog">Library</a></p>
+  <p><a href="#home">News</a></p>
+  <p><a href="#wgpt3">Videos</a></p>
+  <p><a href="#possibility">Reviews</a></p>
+  <p><a href="#features">Shows</a></p>
+  <p><a href="#blog">Wikis</a></p>
   </>
 )
 
