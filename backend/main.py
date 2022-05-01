0cfcd960329ef2c3bd1f46a35e48cfda7f6c4c50
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-04-30 21:39:58 -0400

    April 30, 2022 9:39 PM

--- backend/main.py
+++ backend/main.py
@@ -6,13 +6,12 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 @app.route("/api/articles")
 def articles():
     response = requests.get("https://ign-apis.herokuapp.com/articles")
-    print(response.json().get("data"))
-    return response.json()
+    return response.json().get("data")
 
 @app.route("/api/videos")
 def videos():
     response = requests.get("https://ign-apis.herokuapp.com/videos")
-    return response.json()
+    return response.json().get("data")
 
 if __name__ == "__main__":
     app.run()
