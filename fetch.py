c95121ab87cb02c4fadcb6b1af5610423a992950
SpeedyGonazale5 <peternevarez2@gmail.com>
2022-04-30 21:19:48 -0400

    April 30, 2022 9:19 PM

--- fetch.py
+++ fetch.py
@@ -1,6 +1,6 @@
 import requests
 def thing():
-    response = requests.get("https://ign-apis.herokuapp.com")
+    response = requests.get("https://ign-apis.herokuapp.com/articles")
     print(response.json()) 
 if __name__ == "__main__":
     thing()
